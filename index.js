0df51620d83068535dfd5b6b4c8388b08f0f77a1
yogesh <coder.yogeshkashyap@gmail.com>
2024-02-03 10:33:48 +0530

    working

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000 || process.env.PORT; // You can use any port you prefer
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle GET requests to the root URL
+app.get('/', (req, res) => {
+  // Send the index.html file
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
