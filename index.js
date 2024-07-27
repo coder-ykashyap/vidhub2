d4faa48b6f4aea2edf669880ce1a3b032150b9e7
yogesh <coder.yogeshkashyap@gmail.com>
2024-07-27 21:40:24 +0530

    new api added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000 || process.env.PORT; // You can use any port you prefer
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle GET requests to the root URL
 app.get('/', (req, res) => {
